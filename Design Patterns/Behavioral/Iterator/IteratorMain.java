fad95b0fe06b29c12a20734944a1d5d531e38727
PamodyaMaheshani <pamodyamaheshani2021@gmail.com>
2021-12-12 19:53:36 +0530

    Implemented Iterator design pattern.

---
+++ Design Patterns/Behavioral/Iterator/IteratorMain.java
@@ -0,0 +1,16 @@
+package Iterator;
+
+public class IteratorMain {
+	
+	public static void main(String[] args) {
+		
+		NameRepository namesRepository = new NameRepository();
+
+	      for(Iterator iter = namesRepository.getIterator(); iter.hasNext();){
+	         String name = (String)iter.next();
+	         System.out.println("Name : " + name);
+	         
+	      } 
+	}
+
+}
