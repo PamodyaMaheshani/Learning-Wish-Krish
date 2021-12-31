0efd124d84111318b17983052b8822405173bb2b
PamodyaMaheshani <pamodyamaheshani2021@gmail.com>
2021-12-31 12:29:49 +0530

    Added printing statements

--- StaticHomework/MainClass.java
+++ StaticHomework/MainClass.java
@@ -1,11 +1,18 @@
-class MainClass {
+class MainClass {	
+
+	static {
+		System.out.println("Hi.. I am in the static method in the MainClass");	
+	}
 
 	public static void main(String[] args) {
 
-		Blocks b = new Blocks();
+		System.out.println("Hi.. I am in the main method in the MainClass\n");
+		Blocks b1 = new Blocks();
 		System.out.println("");
-		Blocks b1 = new Blocks("parameterized");
+		Blocks b2 = new Blocks("parameterized");
 	}	
+
+	
 }
 
 	
