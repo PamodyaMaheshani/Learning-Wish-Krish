0fb793bd93a32b62c601ea2924ad8c278e4a9401
PamodyaMaheshani <pamodyamaheshani2021@gmail.com>
2021-12-31 11:58:44 +0530

    Created 2 objects to get the output

---
+++ StaticHomework/Blocks.java
@@ -0,0 +1,19 @@
+class Blocks {
+
+	//constructor
+	Blocks() 
+ 	{
+		System.out.println("Hi.. I am in the constructor block of this Blocks class");
+	}	
+
+	//Static block
+	static
+	{
+		System.out.println("Hi.. I am in the static block of this Blocks class");
+	}
+
+	//empty block
+	{
+		System.out.println("Hi.. I am in the empty block of this Blocks class");
+	}
+}
